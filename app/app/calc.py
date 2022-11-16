479bebfff30f870ed7131dec3883620ef6bb0e19
Aryan Jain <aryan.jain@simplifyvms.com>
2022-11-17 01:27:05 +0530

    Far Commits

---
+++ app/app/calc.py
@@ -0,0 +1,13 @@
+"""
+    Calculator functions
+"""
+
+
+def add(x,y):
+    """ADD x and y and return result."""
+    return x + y
+
+def subtract(x,y):
+    """Subtract x from y"""
+    return y - x
+

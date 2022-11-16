186af3245ac102cfe19cd85cfca2b1b9e8b87052
Aryan Jain <aryan.jain@simplifyvms.com>
2022-11-17 01:45:09 +0530

    Double Commits

--- app/app/calc.py
+++ app/app/calc.py
@@ -3,11 +3,10 @@
 """
 
 
-def add(x,y):
+def add(x, y):
     """ADD x and y and return result."""
     return x + y
 
-def subtract(x,y):
+def subtract(x, y):
     """Subtract x from y"""
     return y - x
-
